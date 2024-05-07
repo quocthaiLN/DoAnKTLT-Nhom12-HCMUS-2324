88e361e2872c5574749340f15f2cb6e7c0d7966e
quocthaiLN <quocthailenguyen41@gmail.com>
2024-05-07 09:25:12 +0700

    update 7/5

--- DoAnKTLT-Nhom12-HCMUS-2324/function.h
+++ DoAnKTLT-Nhom12-HCMUS-2324/function.h
@@ -1,1 +1,61 @@
-#pragma once
+#ifndef function_h
+#define function_h
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct date
+{
+	int day, month, year;
+};
+
+struct taiKhoan
+{
+	string nickName, password;
+};
+
+
+
+
+struct sinhVien
+{
+	string firstName, lastName;
+	taiKhoan tk;
+	int no, MSSV;
+	date birth;
+	int gender;
+	long long socialID;
+	string cls;
+	int semester, schoolYear;
+};
+
+struct nodeSinhVien
+{
+	sinhVien info;
+	nodeSinhVien* next;
+};
+
+struct ListSinhVien
+{
+	nodeSinhVien* pHead,* pTail;
+};
+
+struct giangVien
+{
+	string nickName;
+	string passWord;
+	int no, MSSV;
+	date birth;
+	int gender;
+	long long socialID;
+	string firstName, lastName;
+};
+
+
+
+
+
+
+#endif // !function_h
+
